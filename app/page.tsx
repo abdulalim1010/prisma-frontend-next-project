b0a12d5050dc649ad731fb65677d9c08c9314913
abdulaim1010 <abdulaim243457@gmail.com>
2026-07-20 12:30:06 +0600

    add to the get post and navigaiotn test

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>hello  the next js</div>
+    <div>hello  the next js
+
+<Link href={"/blogs/1"}> blogs</Link>
+
+    </div>
   );
 }
