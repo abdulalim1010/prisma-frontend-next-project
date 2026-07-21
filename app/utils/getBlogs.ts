13eb41ce5fbbfacd82045ab32dac6945b380a8a7
abdulaim1010 <alim.12016032@student.brur.ac.bd>
2026-07-21 23:33:36 +0600

    add to the path

--- app/utils/getBlogs.ts
+++ app/utils/getBlogs.ts
@@ -1,13 +1,30 @@
-import { cacheLife } from "next/cache";
+import { cacheLife, revalidateTag } from "next/cache";
 
 
 export const GetBlogs=async()=>{
-    "use cache"
-     cacheLife('hours')
+    // "use cache"
+    //  cacheLife('hours')
 
-    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts",{
+        method:"POST",
+        cache:"force-cache",
+        next:{
+            revalidate:60*60*24,
+            tags:["posts"]
+        }
+
+    });
 const postData = await res.json();
 console.log(postData)
 return postData
+
+}
+const renewBlogTags=()=>{
+    revalidateTag("post",{
+        expire:60*60*24,
+
+    })
+
+
 
 }
