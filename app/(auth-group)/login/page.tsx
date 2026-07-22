49a101a3e5537e3aedc0c5e0c8011b110c0f41c2
abdulaim1010 <alim.12016032@student.brur.ac.bd>
2026-07-22 16:58:51 +0600

    add to the path

--- app/(auth-group)/login/page.tsx
+++ app/(auth-group)/login/page.tsx
@@ -1,6 +1,8 @@
 import { LoginForm } from "@/components/login-form"
 
 export default function LoginPage() {
+
+    console.log(process.env.BACKEND_API_URL)
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
