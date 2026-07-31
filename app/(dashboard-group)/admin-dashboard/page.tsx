0afc62835fa12e64634f3fc7e193e5a04b39353e
abdulaim1010 <alim.12016032@student.brur.ac.bd>
2026-07-31 10:42:00 +0600

    add to the dashboard

--- app/(dashboard-group)/admin-dashboard/page.tsx
+++ app/(dashboard-group)/admin-dashboard/page.tsx
@@ -1,6 +1,17 @@
-const AdminDashboardPage=()=>{
-    return(<div>
-        Admin dashboard on the page
-    </div>)
-}
-export default AdminDashboardPage
+
+
+
+const AdminDashboardPage = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">
+        Admin Dashboard
+      </h1>
+
+
+      <p>Welcome Admin 👋</p>
+    </div>
+  );
+};
+
+export default AdminDashboardPage;
